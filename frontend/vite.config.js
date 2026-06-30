567f2d06291087e39ae347becbb6e62013ef2fa0
ArizalaRonAndrew <ronandrewarizala18@gmail.com>
2026-06-30 14:43:45 +0800

    Develop Line Chart and Bar Graph

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,17 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-import tailwindcss from "@tailwindcss/vite";
-
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
   test: {
     environment: "jsdom",
     globals: true,
     setupFiles: "./src/tests/setup.js",
   },
-});
 });
